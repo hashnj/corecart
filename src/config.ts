887b479c8bbaac92a356edd03971af82169e5e33
hashnj <joshinikhilesh46@gmail.com>
2025-03-09 08:59:59 +0530

    deploy

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,3 @@
-import dotenv from "dotenv";
-
-dotenv.config(); 
-
-export const B_Url = process.env.BACKEND_URL || "http://localhost:3300";
-export const F_URL = process.env.FRONTEND_URL || "http://localhost:5173";
-export const STRIPE_PUBLIC_KEY = process.env.STRIPE_PUBLIC_KEY || "";
+export const B_Url = import.meta.env.VITE_BACKEND_URL || "http://localhost:3300";
+export const F_URL = import.meta.env.VITE_FRONTEND_URL || "http://localhost:5173";
+export const STRIPE_PUBLIC_KEY = import.meta.env.VITE_STRIPE_PUBLIC_KEY || "";
